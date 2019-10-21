bd78f5b2b51ebfcf7424622dbd059317f3a8a92e
shubham <kumarsinghshubham32111@gmail.com>
2019-10-21 21:58:48 +0530

    d

--- src/x/FactCall.java
+++ src/x/FactCall.java
@@ -1,4 +1,43 @@
 package x;
 
+import java.util.*;
+import java.util.concurrent.*;
+
+class Factorial implements Callable<Integer>{
+    int n;
+
+    Factorial(int n){
+        this.n = n;
+    }
+
+    public Integer call() throws Exception{
+        int f = 1;
+        for(int i=1;i<=n;i++){
+            f*=i;
+        }
+        return f;
+    }
+}
+
 public class FactCall {
+    public static void main(String args[]){
+        ExecutorService e = Executors.newFixedThreadPool(3);
+        List<Future<Integer>> list = new ArrayList<Future<Integer>>();
+        for(int i=0;i<5;i++){
+            Factorial f = new Factorial(i);
+            Future<Integer> fi =  e.submit(f);
+            list.add(fi);
+        }
+        for(Future<Integer> f: list){
+            try{
+                System.out.println(f.get());
+            }catch(InterruptedException i){
+                System.out.println(i);
+            }catch(ExecutionException j){
+                System.out.println(j);
+            }
+
+        }
+        e.shutdown();
+    }
 }
